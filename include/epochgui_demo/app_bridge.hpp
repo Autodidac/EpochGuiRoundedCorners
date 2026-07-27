d21f9934e05bad96c1a2814f08b7fb5a26fc0f9f
Adam Rushford <177971875+Autodidac@users.noreply.github.com>
2026-07-26 23:48:45 -0400

    Add cross-platform renderer bridge

---
+++ include/epochgui_demo/app_bridge.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+    typedef void (*epoch_gui_demo_gl_proc)(void);
+    typedef epoch_gui_demo_gl_proc (*epoch_gui_demo_proc_loader)(const char* name);
+
+    struct epoch_gui_demo_renderer;
+
+    [[nodiscard]] epoch_gui_demo_renderer* epoch_gui_demo_create(void);
+    void epoch_gui_demo_destroy(epoch_gui_demo_renderer* renderer);
+
+    [[nodiscard]] bool epoch_gui_demo_initialize(
+        epoch_gui_demo_renderer* renderer,
+        epoch_gui_demo_proc_loader loader);
+
+    void epoch_gui_demo_resize(
+        epoch_gui_demo_renderer* renderer,
+        int width,
+        int height);
+
+    void epoch_gui_demo_render(epoch_gui_demo_renderer* renderer);
+
+#ifdef __cplusplus
+}
+#endif
